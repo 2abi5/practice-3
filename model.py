c2990ea9cf7861154fe167633c6f724e04ab1369
2abi5 <155565214+2abi5@users.noreply.github.com>
2024-01-04 21:34:39 +0545

    hunca ta aba

---
+++ model.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+pk=pd.DataFrame({"Area":[2600,3000,3200,3600,4000],"Bedrooms":[3,4,3,3,5],"age":[20,15,18,30,8],"price":[550000,565000,610000,595000,760000]})
+pk
